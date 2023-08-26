643bfe91548fd1af3c765bfb0e131b6357e2a4f3
Ryan <ryan@ryannd.com>
2023-08-26 12:37:54 -0500

    Init, added Card, Library, Deck, and ICardCollection

---
+++ DeckRND.Core/Contracts/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckRND.Core.Contracts
+{
+    public class Card
+    {
+
+    }
+}
---
+++ DeckRND.Core/Contracts/Deck.cs
@@ -0,0 +1,13 @@
+﻿using DeckRND.Core.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckRND.Core.Contracts
+{
+    public class Deck : ICardCollection
+    {
+    }
+}
